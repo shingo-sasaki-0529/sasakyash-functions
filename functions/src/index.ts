7b257bdf6914b0569700e7540ae9e255d0c23525
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-22 04:30:40 +0900

    残額を取得するAPIまで実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,13 @@ import * as functions from 'firebase-functions'
 import { fetchCurrentBalance } from './zaim'
 import * as dayjs from 'dayjs'
 
-export const helloWorld = functions.https.onRequest(async (request, response) => {
+/**
+ * 残額を取得する
+ */
+export const balance = functions.https.onRequest(async (request, response) => {
   const year = request.query.year ? Number(request.query.year) : dayjs().year()
   const month = request.query.month ? Number(request.query.month) - 1 : dayjs().month()
-  const payments = await fetchCurrentBalance(year, month, '私費')
+  const paymentType = request.query.paymentType === 'private' ? '私費' : '公費'
+  const payments = await fetchCurrentBalance(year, month, paymentType)
   response.json({ data: payments })
 })

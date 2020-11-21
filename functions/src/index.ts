d84c95aaf6aeb487f2a1c260971684610f556450
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-22 03:15:16 +0900

    既存コードにPrettierを適用

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,7 @@
-import * as functions from "firebase-functions";
-import { fetchPayments } from "./zaim";
+import * as functions from 'firebase-functions'
+import { fetchPayments } from './zaim'
 
 export const helloWorld = functions.https.onRequest(async (_, response) => {
-  const payments = await fetchPayments(
-    new Date("2020/11/01"),
-    new Date("2020/11/30"),
-    "公費"
-  );
-  response.json({ data: payments });
-});
+  const payments = await fetchPayments(new Date('2020/11/01'), new Date('2020/11/30'), '公費')
+  response.json({ data: payments })
+})
--- functions/src/zaim.ts
+++ functions/src/zaim.ts
@@ -1,35 +1,25 @@
-import { config } from "firebase-functions";
-import { Money, MoneyType, PaymentType } from "./types";
-import Zaim from "zaim";
+import { config } from 'firebase-functions'
+import { Money, MoneyType, PaymentType } from './types'
+import Zaim from 'zaim'
 
-const zaimConfig = config().zaim;
+const zaimConfig = config().zaim
 const zaim = new Zaim({
   consumerKey: zaimConfig.key as string,
   consumerSecret: zaimConfig.secret as string,
   accessToken: zaimConfig.token as string,
-  accessTokenSecret: zaimConfig.token_secret as string,
-});
+  accessTokenSecret: zaimConfig.token_secret as string
+})
 
-export const fetchMoneyList = async (
-  startDate: Date,
-  endDate: Date,
-  mode: MoneyType
-) => {
+export const fetchMoneyList = async (startDate: Date, endDate: Date, mode: MoneyType) => {
   const responseData = await zaim.getMoney({
-    start_date: startDate.toISOString().split("T")[0],
-    end_date: endDate.toISOString().split("T")[0],
-    mode: mode,
-  });
-  return JSON.parse(responseData)["money"] as Money[];
-};
+    start_date: startDate.toISOString().split('T')[0],
+    end_date: endDate.toISOString().split('T')[0],
+    mode: mode
+  })
+  return JSON.parse(responseData)['money'] as Money[]
+}
 
-export const fetchPayments = async (
-  startDate: Date,
-  endDate: Date,
-  paymentType: PaymentType
-) => {
-  const payments = await fetchMoneyList(startDate, endDate, "payment");
-  return payments.filter(
-    (payment) => payment.comment.indexOf(paymentType) >= 0
-  );
-};
+export const fetchPayments = async (startDate: Date, endDate: Date, paymentType: PaymentType) => {
+  const payments = await fetchMoneyList(startDate, endDate, 'payment')
+  return payments.filter(payment => payment.comment.indexOf(paymentType) >= 0)
+}

faed87a3e22d70783bbee774eaf789757acede08
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-22 00:28:59 +0900

    HelloWorld関数のメッセージを微修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
 
 export const helloWorld = functions.https.onRequest((_, response) => {
-  response.json({ data: { message: "Hello, World2" } });
+  response.json({ data: { message: "Hello, World from functions!!" } });
 });

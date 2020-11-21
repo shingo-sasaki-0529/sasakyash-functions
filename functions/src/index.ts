b52fe37275a90dfdf32a81be514a08b815ca8ae3
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-22 02:12:19 +0900

    Zaimモジュールを追加し、ユーザー情報を取得できることを確認

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,14 @@
 import * as functions from "firebase-functions";
+import Zaim from "zaim";
 
-export const helloWorld = functions.https.onRequest((_, response) => {
-  response.json({ data: { message: "Hello, World from functions!!" } });
+const zaim = new Zaim({
+  consumerKey: process.env.ZAIM_KEY as string,
+  consumerSecret: process.env.ZAIM_SECRET as string,
+  accessToken: process.env.ZAIM_TOKEN as string,
+  accessTokenSecret: process.env.ZAIM_TOKEN_SECRET as string,
+});
+
+export const helloWorld = functions.https.onRequest(async (_, response) => {
+  const userInfo = await zaim.verify();
+  response.json({ data: JSON.parse(userInfo) });
 });

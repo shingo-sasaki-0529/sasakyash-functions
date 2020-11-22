c13dfcde5135fa165240efb0c121ebc64463d3ef
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-23 06:54:59 +0900

    dailyPaymentAmount API を、リクエストボディてパラメータ指定できるように

--- functions/src/api/dailyPaymentAmount.ts
+++ functions/src/api/dailyPaymentAmount.ts
@@ -4,8 +4,9 @@ import { fetchDailyPaymentAmounts } from '../zaim'
 const cors = require('cors')({ origin: true })
 
 export default functions.https.onRequest(async (request, response) => {
-  const dateFrom = request.query.dateFrom ? dayjs(request.query.dateFrom as string) : dayjs().startOf('month')
-  const dateTo = request.query.dateTo ? dayjs(request.query.dateTo as string) : dateFrom.endOf('month')
+  const params = request.body.data
+  const dateFrom = params.dateFrom ? dayjs(params.dateFrom as string) : dayjs().startOf('month')
+  const dateTo = params.dateTo ? dayjs(params.dateTo as string) : dateFrom.endOf('month')
   const dailyPaymentAmounts = await fetchDailyPaymentAmounts(dateFrom, dateTo)
 
   cors(request, response, () => {

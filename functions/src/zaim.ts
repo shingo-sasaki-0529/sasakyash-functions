f1eeab68dad8ec0bd0a2e409fe4a3a2ea59d166b
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-24 01:39:40 +0900

    支払い一覧をfirestoreにキャッシュする

--- functions/src/zaim.ts
+++ functions/src/zaim.ts
@@ -14,15 +14,38 @@ const zaim = new Zaim({
   accessTokenSecret: Config.token_secret as string
 })
 
+const firebaseAdmin = require('firebase-admin')
+firebaseAdmin.initializeApp()
+const db = firebaseAdmin.firestore()
+
 /**
  * 支払い一覧をAPIから取得する
  */
 export const fetchPaymentList = async (startDate: Dayjs, endDate: Dayjs) => {
-  const responseData = await zaim.getMoney({
+  // Zaim API 用のパラメータを組み立てる
+  const params = {
     start_date: startDate.format('YYYY-MM-DD'),
     end_date: endDate.format('YYYY-MM-DD'),
-    mode: 'payment'
-  })
+    mode: 'payment' as 'payment'
+  }
+  const serializedParams = JSON.stringify(params)
+
+  // リクエストパラメータを元に、キャッシュの有無を確認する
+  const cache = await db.collection('zaim-get-money-cache').doc(serializedParams).get()
+  const cachedData = cache.data()
+
+  // キャッシュがある場合はそれをモデル化してそのまま返却する
+  if (cachedData) {
+    return new PaymentList(cachedData.result)
+  }
+
+  // キャッシュがないので Zaim API を呼び出し、レスポンスをパースする
+  const responseData = await zaim.getMoney(params)
   const rawPaymentList = JSON.parse(responseData)['money'] as Money[]
+
+  // キャッシュを新規作成する
+  await db.collection('zaim-get-money-cache').doc(serializedParams).set({ result: rawPaymentList })
+
+  // モデル化して返却する
   return new PaymentList(rawPaymentList)
 }

aca010c68e027cb585f4b3d4d517ec42b25435fe
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-29 16:22:01 +0900

    fix for eslint

--- functions/src/utils/date.ts
+++ functions/src/utils/date.ts
@@ -5,9 +5,9 @@ type Dayjs = dayjs.Dayjs
  * 指定した期間の日付一覧を取得する
  * ex) ['2020-10-01', '2020-10-02', ... '2020-10-31']
  */
-export const dateRange = (startDate: Dayjs, endDate: Dayjs) => {
-  startDate = startDate.startOf('day')
-  endDate = endDate.startOf('day')
+export const dateRange = (_startDate: Dayjs, _endDate: Dayjs) => {
+  const startDate = _startDate.startOf('day')
+  const endDate = _endDate.startOf('day')
 
   const dateList: Dayjs[] = []
   let iteratorDate: Dayjs = startDate
